8c008dbe992f9ac75b73c50eeff17934481be828
soaresden <54243866+soaresden@users.noreply.github.com>
2020-12-01 17:57:15 +0100

    Released v3.1

--- RecalCopier/00-Menu.Designer.vb
+++ RecalCopier/00-Menu.Designer.vb
@@ -206,9 +206,9 @@ Partial Class Form1
         Me.label3.Location = New System.Drawing.Point(843, 515)
         Me.label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.label3.Name = "label3"
-        Me.label3.Size = New System.Drawing.Size(91, 14)
+        Me.label3.Size = New System.Drawing.Size(98, 14)
         Me.label3.TabIndex = 14
-        Me.label3.Text = "v3 by Soaresden"
+        Me.label3.Text = "v3.1 by Soaresden"
         '
         'Disclaimer
         '
