a843090aafb1b93ca4b5547f87bf549c76033091
Denis SOARES <soaresden@gmail.com>
2021-03-06 11:01:44 +0100

    releasing v5.4

--- RecalCopier/00-Menu.Designer.vb
+++ RecalCopier/00-Menu.Designer.vb
@@ -160,9 +160,9 @@ Partial Class Form1
         Me.version.Location = New System.Drawing.Point(128, 51)
         Me.version.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.version.Name = "version"
-        Me.version.Size = New System.Drawing.Size(27, 14)
+        Me.version.Size = New System.Drawing.Size(28, 14)
         Me.version.TabIndex = 14
-        Me.version.Text = "v5.3"
+        Me.version.Text = "v5.4"
         '
         'Disclaimer
         '
